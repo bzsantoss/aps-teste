f36aad05ee6b837b153e1ba8e829c33c49e6a59f
bzsantoss <150487309+bzsantoss@users.noreply.github.com>
2023-11-18 12:01:13 -0300

    Create alg.h

---
+++ alg.h
@@ -0,0 +1,15 @@
+#ifndef ALG_H
+#define ALG_H
+
+#include <time.h>
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void MyQuickSort(int *array, int size);
+void MySelectionSort(int *array, int size);
+void MyBubbleSort(int *array, int size);
+void MyDataGenerator();
+
+#endif  // ALG_H
